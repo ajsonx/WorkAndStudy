1c3e688b5ef738d45fca9140545edda6d3e919bb
ajsonx <577429696@qq.com>
2019-03-11 19:49:12 +0800

    PHP代码面试题实例

---
+++ Questions.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * 经典面试题
+ * Created by PhpStorm.
+ * User: ajsonx
+ * Date: 2019/2/12
+ * Time: 16:15
+ */
+
+/*
+ * 最里层打印 $i 的值 111
+ * 外层打印 $i 的类型 3 (INT(3) )
+ * 再有就是打印 3 的类型 1 (INT(1) )
+ */
+$i='111';
+printf("%d\n",printf("%d",$i));
+printf("%d\n",printf("%d",printf("%d",printf("%d",$i))));
+var_dump(printf("%d",$i));
+
+/*
+ * 函数名不区分大小写
+ */
+$a = 'ab';
+echo gettype($a);
+echo GETTYPe($a);
+
+/*
+ * 变量未赋值都是null
+ * ++ 变成 1
+ */
+static $count;
+echo $count; //未赋值是Null 什么也不输出
+$count++;  //变成1
+echo $count.PHP_EOL;
+
+/*
+ * str_split($str,$length)
+ * 原字符串，分割到每个数组的长度 默认是1
+ * 把字符串分割到数组中
+ */
+$str = "helloworld";
+$arr = str_split($str,1);
+foreach ($arr as $v){
+    printf("%s",$v);
+}echo PHP_EOL;
+
+/*
+ * 身份证正则表达式
+ * 限制 首位不为0 1800-2300 日期
+ */
+$str = '350182199706312035';
+$pattern = '/^[1-9]\d{5}(18|19|2[0-3])\d{2}(0[1-9]|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/is';
+preg_match($pattern,$str,$match);
+var_dump($match);
+$str1 = '350182';
+$pattern1 = '/^(?:[1-9]\d{5})$/';
+var_dump(preg_match($pattern1,$str1,$matchs));
+var_dump($matchs);
+
+/*
+ * 正则表达式反向引用
+ * preg_replace($pattern,$replacement,$str);
+ * 正则，替换串，原字符串
+ */
+$str = "April 15,2003";
+$pattern = '/(\w+) (\d+),(\d+)/i';
+$replacement = '${1}22,$3';
+echo preg_replace($pattern,$replacement,$str);
+
+/*
+ * 输入格式为"get-element-by-id"的任意字符串，将其转换为驼峰命名的字符串。
+ * 例如getElementById
+ * strtok($str,$split)
+ * 分割同一个字符串，仅需在第一次调用 $str 参数，后续仅需要 split 参数。返回
+ * 这是因为它清楚自己在当前字符串中所在的位置。
+ * 如需分割一个新的字符串，请再次调用带 string 参数的 strtok()
+ */
+$str = 'get-element-by-id';
+
+$temp = strtok($str,'-');
+
+$ans = array();
+while($temp !== false){
+    array_push($ans,$temp);
+    $temp = strtok('-');
+}
+var_dump($ans);
+
+for($i = 1; $i < sizeof($ans);$i++){
+    $ans[$i] = ucfirst($ans[$i]);
+}
+
+$str_ans = implode($ans);
+
+echo $str_ans.PHP_EOL;
+
+/*
+ *
+ */
